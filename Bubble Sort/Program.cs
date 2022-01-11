dd53ccb4b2fd95c2d90e63e4fbb7b2204f34310c
Charlie Kingsland <18499854+ChopsKingsland@users.noreply.github.com>
2022-01-11 15:09:53 +0000

    fixed

--- Bubble Sort/Program.cs
+++ Bubble Sort/Program.cs
@@ -8,7 +8,7 @@ namespace Bubble_Sort
         static void Main(string[] args)
         {
             // declare array to sorted
-            int[] unsorted = new int[] { 12, 56, 2, 4, 6, 8, 1, 23, 5, 19, 18, 43, 32, 7 };
+            int[] unsorted = new int[] { 12, 56, 2, 4, 6, 8, 1, 23, 5, 19, 18, 43, 32, 7, 2 };
 
             //empty variables for later use
             int temp = 0;
@@ -20,7 +20,7 @@ namespace Bubble_Sort
                 swapped = false;
 
                 // loop through all elements of array
-                for (int i = 0; i < unsorted.Length - 2; i++)
+                for (int i = 0; i < unsorted.Length - 1; i++)
                 {
                     // if in wrong order
                     if (unsorted[i] > unsorted[i + 1])
