c99ae7306c962ce4d48d4820cca23d6055225906
Charlie Kingsland <18499854+ChopsKingsland@users.noreply.github.com>
2022-01-11 15:00:55 +0000

    all

---
+++ Bubble Sort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // declare array to sorted
+            int[] unsorted = new int[] { 12, 56, 2, 4, 6, 8, 1, 23, 5, 19, 18, 43, 32, 7 };
+
+            //empty variables for later use
+            int temp = 0;
+            bool swapped = true;
+
+            //loop while is swapped
+            while (swapped == true)
+            {
+                swapped = false;
+
+                // loop through all elements of array
+                for (int i = 0; i < unsorted.Length - 2; i++)
+                {
+                    // if in wrong order
+                    if (unsorted[i] > unsorted[i + 1])
+                    {
+                        // swap values
+                        temp = unsorted[i];
+                        unsorted[i] = unsorted[i + 1];
+                        unsorted[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", unsorted));
+
+
+        }
+    }
+}
